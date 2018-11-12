a7f2e2e6edda4370cc1d132320d85a2a1a2cd97b
elrohil <wieslaw.stanek@spinney.io>
2018-11-12 13:35:14 +0100

    Sample app

---
+++ src/app/index.js
@@ -0,0 +1,18 @@
+/* USER CODE START */
+
+while (true) {
+  ledOff();
+  sleep(5000);
+  ledOn();
+  sleep(50);
+  ledOff();
+  sleep(50);
+  ledOn();
+  sleep(50);
+  ledOff();
+  sleep(5000);
+  ledOn();
+}
+
+
+/* USER CODE END */
