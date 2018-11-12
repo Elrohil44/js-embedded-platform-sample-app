549839919d5f9d2638c948f1f9d234376574e13a
Wiesław Stanek <wieslaw.stanek@spinney.io>
2018-11-12 22:29:44 +0100

    Added sample function

--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,10 @@
 /* USER CODE START */
 
-while (true) {
-  ledOff();
-  sleep(5000);
-  ledOn();
-  sleep(50);
-  ledOff();
-  sleep(50);
-  ledOn();
-  sleep(50);
-  ledOff();
-  sleep(5000);
-  ledOn();
-}
-
+var gpio = require('gpio');var sw = false;
+gpio.setup(gpio.PORTB, gpio.PIN0, gpio.MODE_OUT_PP, gpio.NOPULL);
+gpio.setup(gpio.PORTB, gpio.PIN7, gpio.MODE_OUT_PP, gpio.NOPULL);
+gpio.setup(gpio.PORTC, gpio.PIN13, gpio.MODE_IN, gpio.PULLDOWN);
+setInterval(function(){ gpio.set(gpio.PORTB, gpio.PIN0, sw); sw = !sw; }, 250);
+setInterval(function(){ var isSet = gpio.get(gpio.PORTC, gpio.PIN13); gpio.set(gpio.PORTB, gpio.PIN7, isSet); }, 150);;
 
 /* USER CODE END */
